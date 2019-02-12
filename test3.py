26fdc6856604e62e21ff535dcb5ba41db5fe849d
Anna Branco <abranco@atsistemas.com>
2019-02-12 14:55:35 +0100

    upd: more studies

---
+++ test3.py
@@ -0,0 +1,48 @@
+# String Formats
+# Same thing for string formats: use them in some strings to know what they do.
+# %d
+# %i
+# %o
+# %u
+# %x
+# %X
+# %e
+# %E
+# %f
+# %F
+# %g
+# %G
+
+# Operators
+# Some of these may be unfamiliar to you, but look them up anyway. Find out what they do, and if
+# you still can’t fi gure it out, save it for later.
+# +
+# -
+# *
+# **
+# /
+# //
+# %
+# <
+# >
+# <=
+# >=
+# ==
+# !=
+# < >
+# ()
+# []
+# {}
+# @
+# •,
+# •:
+# .
+# =
+# •
+# +=
+# - =
+# *=
+# /=
+# //=
+# %=
+# **=
