50d453ccdedb2800574df87c0e54451f9112f638
Anna <anna.branco@outlook.com>
2019-02-18 12:49:36 +0100

    enh: basic defensive mechanics done & print() adjustments

--- game/mechanics/combat/defend.py
+++ game/mechanics/combat/defend.py
@@ -1,6 +1,6 @@
 from random import randint
 from mechanics.global_mechanics import modifiers
-from db.hero import hero
+from db.hero import Hero
 import actions
 import cinematics
 from mechanics.combat import combat_mechanics
@@ -10,8 +10,8 @@ from core.config import print
 def defend(enemy, bonus=0, focus_on_defense=False):
 
     if focus_on_defense:
-        print('Hero stands on a defensive stance')
+        print('Hero stands on a defensive stance.')
 
-    difficult = hero['defense'] + bonus
+    difficult = Hero['defense'] + bonus
     base_attack = enemy['attack']
-    combat_mechanics.dispatch_attack(difficult, base_attack, enemy, hero)
+    combat_mechanics.dispatch_attack(difficult, base_attack, enemy, Hero)
