aff0890a93e415a7918af442389624365d857775
Anna <anna.branco@outlook.com>
2019-02-18 11:36:08 +0100

    add: call enemy by argv on start

--- game/start.py
+++ game/start.py
@@ -4,12 +4,11 @@ from db import enemies
 from db.hero import hero
 from sys import argv
 
-# WIP: default value is not working
 argv = argv[1:]
 if len(argv) == 0:
-	enemy = ugly_monster
+    enemy = ugly_monster
 else:
-	enemy_called = argv[0]
-	enemy = enemies[{enemy_called}]
+    enemy_called = argv[0]
+    enemy = getattr(enemies, enemy_called)
 
 cinematics.start_encounter(enemy)
