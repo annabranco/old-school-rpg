a8bc8169b520465612d829e0bb904f638fa58223
Anna Branco <annya.branco@gmail.com>
2019-02-25 23:52:18 +0100

    enh: enemy status changed to npc status

--- game/db/status.py
+++ game/db/status.py
@@ -3,7 +3,7 @@ status_hero = {
 		'feeling great': 6,
 		'feeling fine': 5,
 		'tired': 3,
-		'hurt': 3,
+		'lightly wounded': 3,
 		'unsecure': 3,
 		'scared': 2,
 		'wounded': 2,
@@ -16,19 +16,19 @@ status_hero = {
 # think about bonuses according to status
 # think about system to roll fear reactions
 
-status_enemy = {
+status_npc = {
 		'terrifying': 7,
 		'scary': 6,
 		'dangerous': 5,
 		'misterious': 4,
 		'angry': 4,
 		'calm': 3,
-		'hurt': 3,
+		'lightly wounded': 3,
 		'tired': 3,
 		'peaceful': 2,
 		'scared': 2,
 		'wounded': 2,
 		'panicked': 1,
-		'severily wounded': 1
+		'severily wounded': 1,
 		'dead': 0
 }
