09bf7e5e56036748673d704dbe53892394c03bed
Anna Branco <annya.branco@gmail.com>
2019-02-26 00:00:37 +0100

    add: implemented simultaneous attacks

--- game/mechanics/combat/attack/__init__.py
+++ game/mechanics/combat/attack/__init__.py
@@ -1,15 +1,16 @@
-from core.config import *
 from db.hero import Hero
-from mechanics.global_mechanics.rolls.rolls import roll_dices
+from mechanics.global_mechanics.rolls import roll_dices
 from mechanics.combat import combat_mechanics
 from mechanics.combat.combat_rounds import combat_rounds
 from db import fails
+from mechanics.combat import initiative
+from core.config import *
+
 
 # define texts by types of weapons
 
 
 def attack(enemy, bonus=0, surprise_attack=False):
-
 		combat_rounds.took_action['Hero'] = True
 
 		if surprise_attack == True:
@@ -21,16 +22,23 @@ def attack(enemy, bonus=0, surprise_attack=False):
 		results_number, results_text = roll_dices(base_attack, difficult, f'Attacking {enemy["name"]}')
 
 		if results_text == 'epic':
-				print('Epic success on attack')
 				combat_mechanics.damage(results_number, Hero, enemy)
 		elif results_text == 'decisive' or results_number > 0:
 				combat_mechanics.damage(results_number, Hero, enemy)
 		elif results_text == 'critical':
-				print('Critical fail on attack')
 				fails.disastrous_fail_on_attack(results_number, Hero, enemy)
 		elif results_text == 'disastrous':
-				print('Disastrous fail on attack')
 				fails.disastrous_fail_on_attack(results_number, Hero, enemy)
 		else:
 				combat_mechanics.missed(Hero, enemy)
 
+def simultaneous_attack(enemy):
+		results_number_hero, results_text_hero = roll_dices(Hero['attack'], enemy['defense'], f'Attacking {enemy["name"]}')
+		results_number_enemy, results_text_enemy = roll_dices(enemy['attack'], Hero['defense'], f'{enemy["name"]}\'s attack')
+
+		if (results_number_hero <= 0 and results_number_enemy <= 0):
+				mechanics_block()
+				print_cinematics(f'You and {enemy["name"]} attack at the same time and your blows block each other.')
+				initiative.initiative(enemy, 0)
+		else:
+				combat_mechanics.simultaneous_damage(results_number_hero, Hero, results_number_enemy, enemy)
