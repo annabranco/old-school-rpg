a74a1ff00f18564582e20f86dbe8512d1f2d5e8a
Anna Branco <annya.branco@gmail.com>
2019-02-27 10:54:21 +0100

    wip: Classes system

--- core/characters/__init__.py
+++ core/characters/__init__.py
@@ -1,59 +1,76 @@
 from math import ceil
 
-
 class Character(object):
-    def __init__(self, name, type, race):
-        self.name = name
-        self.type = type
-        self.race = ''
-        self.weapon = {'name': '', 'bonus': 0}
-        self.shield = {'name': '', 'bonus': 0}
-        self.armor = {'name': '', 'bonus': 0}
-        self.inventory = []
-        self.attack = 0
-        self.defense = 0
-        self.full_hp = 0
-        self.hp = 0
-        self.status = ''
+		def __init__(self, name, type, race):
+				self.name = name
+				self.type = type
+				self.race = ''
+				self.weapon = {'name': '', 'bonus': 0}
+				self.shield = {'name': '', 'bonus': 0}
+				self.armor = {'name': '', 'bonus': 0}
+				self.inventory = []
+				self.attack = 0
+				self.defense = 0
+				self.full_hp = 0
+				self.hp = 0
+				self.status = ''
 
-    def change_status(self):
-        if self.hp <= 0:
-            self.status = 'dead'
-        elif self.hp == self.full_hp:
-            self.status = 'well'
-        elif self.hp <= ceil(self.full_hp / 3):
-            self.status = 'severily wounded'
-        elif self.hp <= ceil(self.full_hp * 2 / 3):
-            self.status = 'wounded'
-        else:
-            self.status = 'lightly wounded'
+		def change_status(self):
+				if self.hp <= 0:
+						self.status = 'dead'
+				elif self.hp == self.full_hp:
+						self.status = 'well'
+				elif self.hp <= ceil(self.full_hp / 3):
+						self.status = 'severily wounded'
+				elif self.hp <= ceil(self.full_hp * 2 / 3):
+						self.status = 'wounded'
+				else:
+						self.status = 'lightly wounded'
 
-    def take_damage(self, damage):
-        self.hp = self.hp - damage
-        self.change_status()
+		def take_damage(self, damage):
+				self.hp = self.hp - damage
+				if self.type == 'Player':
+						print(f'You take {damage} damage.')
+				else:
+						print(f'{self.name} takes {damage} damage.')
 
-    def get_status(self):
-        print(f'You are {self.status}')
+				self.get_hp()
+				self.change_status()
 
-    def get_hp(self):
-        print(f'{self.name}\'s current HP: {self.hp}')
+		def get_status(self):
+				if self.type == 'Player':
+						print(f'You are {self.status}')
+				else:
+						print(f'{self.name} is {self.status}.')
 
+		def get_hp(self):
+				if self.type == 'Player':
+						print(f'Your current HP: {self.hp}')
+				else:
+						print(f'{self.name}\'s current HP: {self.hp}.')
 
 class Player(Character):
+		def __init__(self, name='Hero', race='human'):
+				super(Player, self).__init__(name, 'Player', race)
+				self.status = 'well'
 
-    def __init__(self, name='Hero', race='human'):
-        super(Player, self).__init__(name, 'Player', race)
-        self.status = 'well'
+		def get_item(self, item):
+				self.inventory.append(item)
 
-    def get_item(self, item):
-        self.inventory.append(item)
+class NPC(Character):
+		def __init__(self, name='Ugly Monster', race='humanoid'):
+				super(NPC, self).__init__(name, 'NPC', race)
 
 
 Hero = Player('Anna', 'elf')
 Hero.full_hp = Hero.hp = 6
+Orc = NPC('Orc', 'orc')
+Orc.full_hp = Orc.hp = 6
+
 Hero.get_item('sword')
 long_sword = {'name': 'Long sword', 'bonus': 2}
 Hero.get_item(long_sword)
 Hero.take_damage(6)
-Hero.get_hp()
 Hero.get_status()
+Orc.take_damage(6)
+Orc.get_status()
---
+++ temp.py
@@ -0,0 +1,101 @@
+from math import ceil
+
+class Character(object):
+	def __init__(self, name, type, race):
+		self.name = name
+		self.type = type
+		self.race = ''
+		self.weapon = {'name': '', 'bonus': 0}
+		self.shield = {'name': '', 'bonus': 0}
+		self.armor = {'name': '', 'bonus': 0}
+		self.inventory = []
+		self.attack = 0
+		self.defense = 0
+		self.full_hp = 0
+		self.hp = 0
+		self.status = ''
+
+	def change_status(self):
+		if self.hp <= 0:
+			self.status = 'dead'
+		elif self.hp == self.full_hp:
+			self.status = 'well'
+		elif self.hp <= ceil(self.full_hp / 3):
+			self.status = 'severily wounded'
+		elif self.hp <= ceil(self.full_hp * 2 / 3):
+			self.status = 'wounded'
+		else:
+			self.status = 'lightly wounded'
+
+	def take_damage(self, damage):
+		self.hp = self.hp - damage
+		if self.type == 'Player':
+			print(f'You take {damage} damage.')
+		else:
+			print(f'{self.name} takes {damage} damage.')
+		self.declare_hp()
+		self.change_status()
+
+	def declare_status(self):
+		if self.type == 'Player':
+			print(f'You are {self.status}')
+		else:
+			print(f'{self.name} is {self.status}.')
+
+	def declare_hp(self):
+		if self.type == 'Player':
+			print(f'Your current HP: {self.hp}')
+		else:
+			print(f'{self.name}\'s current HP: {self.hp}.')
+
+	def declare_inventory(self):
+		print(f'Your inventory:')
+		# return self.inventory
+		for item in self.inventory:
+			print(type(item))
+			print(type(item) == dict)
+			if type(item) == dict:
+				print(f'\t- {item["name"]}')
+			else:
+				print(f'\t- {item}')
+
+
+class Player(Character):
+	def __init__(self, name='Hero', race='human'):
+		super(Player, self).__init__(name, 'Player', race)
+		self.status = 'well'
+
+	def get_item(self, item):
+		if type(item) == dict:
+			print(f'You get {item["name"]}.')
+		else:
+			print(f'You get {item}.')
+		self.inventory.append(item)
+
+	def drop_item(self, item):
+		self.inventory.pop(item)
+
+class NPC(Character):
+	def __init__(self, name='Ugly Monster', race='humanoid'):
+		super(NPC, self).__init__(name, 'NPC', race)
+
+
+Hero = Player('Anna', 'elf')
+Orc = NPC('Orc', 'orc')
+
+# ---- DAMAGE
+# Hero.full_hp = Hero.hp = 6
+# Hero.take_damage(2)
+# Hero.declare_status()
+# Orc.full_hp = Orc.hp = 6
+# Orc.take_damage(6)
+# Orc.declare_status()
+
+# ---- GET ITEMS
+Hero.get_item('arrows')
+Hero.get_item({'name': 'book'})
+long_sword = {'name': 'Long sword', 'bonus': 2}
+Hero.get_item(long_sword)
+Hero.inventory
+Hero.declare_inventory()
+
