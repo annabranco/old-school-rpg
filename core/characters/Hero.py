abda9daa55fe1d43854e70c69584965d9dd1392a
Anna Branco <annya.branco@gmail.com>
2019-02-27 18:22:41 +0100

    add: Hero definition by Class

--- core/characters/Hero.py
+++ core/characters/Hero.py
@@ -1,1 +1,11 @@
-from core.characters import Character
+from core.characters import Player
+
+Hero = Player('Hero', 'human')
+Hero.weapon = {'name': 'short sword', 'type': 'blade', 'bonus': 0}
+Hero.inventory = [{'name': 'food', 'quantity': 10}]
+Hero.attack = 2
+Hero.defense = 1
+Hero.full_hp = 3
+Hero.speed = 1
+Hero.hp = 3
+Hero.status = 'well'
